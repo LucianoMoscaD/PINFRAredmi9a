6294de1c1d4dc20bfb952ad78b4c9e19d62f8d7f
LucianoMosca <lmd62018@gmail.com>
2023-08-26 16:50:20 -0300

    se agregan validaciones a los campos de creacion de usuario

--- JavaSource/com/capa1presentacion/GestionUsuario.java
+++ JavaSource/com/capa1presentacion/GestionUsuario.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -40,7 +41,7 @@ public class GestionUsuario implements Serializable {
 
 	@Inject
 	UsuariosDAO usuarioDAO;
-	
+
 	@Inject
 	NavigationBean navegacion;
 
@@ -67,24 +68,32 @@ public class GestionUsuario implements Serializable {
 	public GestionUsuario() {
 		super();
 	}
-	
+
 	public void crearAnalista() {
 
 		Usuario usuarioNuevo;
 		try {
 			usuarioSeleccionado.setTipo(1);
-			usuarioNuevo = (Usuario) persistenciaBean.agregarUsuario(usuarioSeleccionado);
-			
-			// actualizamos id
-			Long nuevoId = usuarioNuevo.getId();
-			
-			// vaciamos empleadoSeleccionado como para ingresar uno nuevo
-			usuarioSeleccionado = new Usuario();
+			if (validarDatosBasicosUsuario(usuarioSeleccionado).isEmpty()) {
+				usuarioNuevo = (Usuario) persistenciaBean.agregarUsuario(usuarioSeleccionado);
 
-			// mensaje de actualizacion correcta
-			FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Se ha agregado un nuevo Analista con id:" + nuevoId.toString(), "");
-			FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+				// actualizamos id
+				Long nuevoId = usuarioNuevo.getId();
+
+				// vaciamos empleadoSeleccionado como para ingresar uno nuevo
+				usuarioSeleccionado = new Usuario();
+
+				// mensaje de actualizacion correcta
+				FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Se ha agregado un nuevo Analista con id:" + nuevoId.toString(), "");
+				FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+			} else {
+				for (String error : validarDatosBasicosUsuario(usuarioSeleccionado)) {
+
+					FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", error);
+					FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+				}
+			}
 		} catch (PersistenciaException e) {
 
 			Throwable rootException = ExceptionsTools.getCause(e);
@@ -94,6 +103,9 @@ public class GestionUsuario implements Serializable {
 			FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg1, msg2);
 			FacesContext.getCurrentInstance().addMessage(null, facesMsg);
 
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 
@@ -104,16 +116,24 @@ public class GestionUsuario implements Serializable {
 		Usuario usuarioNuevo;
 		try {
 			usuarioSeleccionado.setTipo(2);
-			usuarioNuevo = (Usuario) persistenciaBean.agregarUsuario(usuarioSeleccionado);
-			// actualizamos id
-			Long nuevoId = usuarioNuevo.getId();
-			// vaciamos empleadoSeleccionado como para ingresar uno nuevo
-			usuarioSeleccionado = new Usuario();
+			if (validarDatos(usuarioSeleccionado).isEmpty()) {
+				usuarioNuevo = (Usuario) persistenciaBean.agregarUsuario(usuarioSeleccionado);
+				// actualizamos id
+				Long nuevoId = usuarioNuevo.getId();
+				// vaciamos empleadoSeleccionado como para ingresar uno nuevo
+				usuarioSeleccionado = new Usuario();
 
-			// mensaje de actualizacion correcta
-			FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Se ha agregado un nuevo Alumno con id:" + nuevoId.toString(), "");
-			FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+				// mensaje de actualizacion correcta
+				FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Se ha agregado un nuevo Alumno con id:" + nuevoId.toString(), "");
+				FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+			} else {
+				for (String error : validarDatos(usuarioSeleccionado)) {
+
+					FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", error);
+					FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+				}
+			}
 		} catch (PersistenciaException e) {
 
 			Throwable rootException = ExceptionsTools.getCause(e);
@@ -128,20 +148,28 @@ public class GestionUsuario implements Serializable {
 
 		}
 	}
+
 	public void crearTutor() {
 		Usuario usuarioNuevo;
 		try {
 			usuarioSeleccionado.setTipo(3);
-			usuarioNuevo = (Usuario) persistenciaBean.agregarUsuario(usuarioSeleccionado);
-			// actualizamos id
-			Long nuevoId = usuarioNuevo.getId();
-			// vaciamos empleadoSeleccionado como para ingresar uno nuevo
-			usuarioSeleccionado = new Usuario();
+			if (validarDatos(usuarioSeleccionado).isEmpty()) {
+				usuarioNuevo = (Usuario) persistenciaBean.agregarUsuario(usuarioSeleccionado);
+				// actualizamos id
+				Long nuevoId = usuarioNuevo.getId();
+				// vaciamos empleadoSeleccionado como para ingresar uno nuevo
+				usuarioSeleccionado = new Usuario();
 
-			// mensaje de actualizacion correcta
-			FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,
-					"Se ha agregado un nuevo Tutor con id:" + nuevoId.toString(), "");
-			FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+				// mensaje de actualizacion correcta
+				FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+						"Se ha agregado un nuevo Tutor con id:" + nuevoId.toString(), "");
+				FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+			} else {
+				for (String error : validarDatos(usuarioSeleccionado)) {
+					FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", error);
+					FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+				}
+			}
 		} catch (PersistenciaException e) {
 
 			Throwable rootException = ExceptionsTools.getCause(e);
@@ -157,14 +185,9 @@ public class GestionUsuario implements Serializable {
 		}
 	}
 
-
 	public void actualizarUsuario() throws PersistenciaException, IOException {
 		if (usuarioAModificar != null) {
-			System.out.println("en actualizarUsuario --> "+ usuarioAModificar.toString());
 			persistenciaBean.agregarUsuario(usuarioAModificar);
-			
-			System.out.println("en actualizarUsuario post persistencia --> "+ usuarioAModificar.toString());
-
 			navegacion.goToBienvenida();
 			usuarioAModificar = null;
 		}
@@ -174,7 +197,7 @@ public class GestionUsuario implements Serializable {
 		try {
 
 			usuarioAModificar = persistenciaBean.obtenerUsuarioPorCedula(selectedUserCedula);
-	    	System.out.println("activo en cargarUsuarioAModificar --> " + usuarioAModificar.getActivo() );
+			System.out.println("activo en cargarUsuarioAModificar --> " + usuarioAModificar.getActivo());
 
 			selectedUserCedula = null;
 
@@ -247,13 +270,12 @@ public class GestionUsuario implements Serializable {
 		List<UsuarioDTO> listaUsuarios = persistenciaBean.buscarUsuarios();
 		return listaUsuarios;
 	}
-	
+
 	public List<UsuarioDTO> buscarTutores() {
 		List<UsuarioDTO> listaUsuarios = persistenciaBean.buscarTutores();
 		return listaUsuarios;
 	}
 
-	
 	public List<UsuarioDTO> buscarAlumnos() {
 		List<UsuarioDTO> listaUsuarios = persistenciaBean.buscarAlumnos();
 		return listaUsuarios;
@@ -275,12 +297,14 @@ public class GestionUsuario implements Serializable {
 	public void mostrarTutores() throws PersistenciaException {
 		usuarios = buscarTutores();
 	}
-	
+
 	public void mostrarUsuarios() throws PersistenciaException {
 		usuarios = buscarUsuarios();
 	}
+
 	public UsuarioDTO login() throws PersistenciaException {
-		UsuarioDTO usuario = persistenciaBean.login(usuarioSeleccionado.getNombreDeUsuario(),usuarioSeleccionado.getPassword());
+		UsuarioDTO usuario = persistenciaBean.login(usuarioSeleccionado.getNombreDeUsuario(),
+				usuarioSeleccionado.getPassword());
 		return usuario;
 	}
 
@@ -320,4 +344,113 @@ public class GestionUsuario implements Serializable {
 		this.fromAlumnoPage = fromAlumnoPage;
 	}
 
+	private List<String> validarDatosBasicosUsuario(Usuario usuarioSeleccionado2) {
+		List<String> errores = new ArrayList<>();
+
+			if (usuarioSeleccionado.getNombre1() == null || usuarioSeleccionado.getNombre1().length() < 2
+					|| usuarioSeleccionado.getNombre1().length() > 30
+					|| usuarioSeleccionado.getNombre1().matches(".*\\d.*")) {
+				errores.add("Debe completar el campo “Primer nombre” con de 2 a 30 caracteres alfabéticos");
+			}
+			if (usuarioSeleccionado.getNombre2().matches(".*\\d.*")) {
+				errores.add("Debe completar el campo “Segundo nombre” con caracteres alfabéticos");
+			}
+			if (usuarioSeleccionado.getApellido1() == null || usuarioSeleccionado.getApellido1().length() < 2
+					|| usuarioSeleccionado.getApellido1().length() > 30
+					|| usuarioSeleccionado.getApellido1().matches(".*\\d.*")) {
+				errores.add("Debe completar el campo “Primer apellido” con de 2 a 30 caracteres alfabéticos");
+			}
+			if (usuarioSeleccionado.getApellido2().matches(".*\\d.*")) {
+				errores.add("Debe completar el campo “Segundo apellido” con caracteres alfabéticos");
+			}
+
+			if (usuarioSeleccionado.getDocumento() == null || usuarioSeleccionado.getDocumento().length() < 8
+					|| usuarioSeleccionado.getDocumento().length() > 8
+					|| !usuarioSeleccionado.getDocumento().matches("^\\d+$")) {
+				errores.add("Debe completar el campo “Documento” con 8 caracteres numéricos, sin guiones o puntos");
+			}
+
+			// FECHA DE NACIMIENTO
+			if (usuarioSeleccionado.getFechaNac() == null) {
+				errores.add("Debe completar el campo “Fecha de nacimiento” ");
+			}
+
+			if (usuarioSeleccionado.getEmailPersonal() == null || !usuarioSeleccionado.getEmailPersonal()
+					.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+				errores.add("Debe completar el campo “Email Personal” con un mail valido");
+			}
+
+			if (usuarioSeleccionado.getTelefono() == null || usuarioSeleccionado.getTelefono().length() < 4
+					|| usuarioSeleccionado.getTelefono().length() > 9
+					|| !usuarioSeleccionado.getTelefono().matches("^\\d+$")) {
+				errores.add(
+						"Debe completar el campo “Teléfono” con de 4 a 9 caracteres numéricos, sin guiones o puntos");
+			}
+
+			if (usuarioSeleccionado.getLocalidad() == null || usuarioSeleccionado.getLocalidad().length() < 2
+					|| usuarioSeleccionado.getLocalidad().length() > 30
+					|| usuarioSeleccionado.getLocalidad().matches(".*\\d.*")) {
+				errores.add("Debe completar el campo “Localidad” con de 2 a 30 caracteres alfabéticos");
+			}
+
+			if (usuarioSeleccionado.getDepartamento() == null || usuarioSeleccionado.getDepartamento().length() < 2
+					|| usuarioSeleccionado.getDepartamento().length() > 30
+					|| usuarioSeleccionado.getDepartamento().matches(".*\\d.*")) {
+				errores.add("Debe completar el campo “Departamento” con de 2 a 30 caracteres alfabéticos");
+			}
+
+			if (usuarioSeleccionado.getEmailInstitucional() == null || !usuarioSeleccionado.getEmailInstitucional()
+					.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+				errores.add("Debe completar el campo “Email Institucional” con un mail valido");
+			}
+
+			if (usuarioSeleccionado.getPassword() == null
+					|| !usuarioSeleccionado.getPassword().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,20}$")) {
+				errores.add(
+						"Debe completar el campo “Contraseña” con 6 a 20 caracteres que contengan al menos una minúscula, una mayúscula y un número");
+			}
+		return errores;
+	}
+	
+	private List<String> validarDatosUsuarioTutor(Usuario usuarioSeleccionado2) {
+		List<String> errores = validarDatosBasicosUsuario(usuarioSeleccionado2);
+		
+		if(usuarioSeleccionado2.getAreaTutor() == null || usuarioSeleccionado2.getAreaTutor().length() < 2) {
+			errores.add("Debe completar el campo “Área a la que pertenece como Tutor”");
+		}
+		
+		if(usuarioSeleccionado2.getRolTutor() == null || usuarioSeleccionado2.getRolTutor().length() < 2) {
+			errores.add("Debe completar el campo “Rol como Tutor”");
+		}
+		
+		return errores;
+	}
+	
+	private List<String> validarDatosUsuarioAlumno(Usuario usuarioSeleccionado2) {
+		List<String> errores = validarDatosBasicosUsuario(usuarioSeleccionado2);
+		
+		if(usuarioSeleccionado2.getAnioIngreso() == null || usuarioSeleccionado2.getAnioIngreso().length() < 2) {
+			errores.add("Debe completar el campo “Año de ingreso a la carrera”");
+		}
+		return errores;
+	}
+	
+	private List<String> validarDatos(Usuario usuario){
+		List<String> errores = new ArrayList<>();
+		
+		if(usuario!=null) {
+			if(usuario.getTipo() == 1) {
+				errores = validarDatosBasicosUsuario(usuario);
+			}
+			
+			if(usuario.getTipo() == 2) {
+				errores = validarDatosUsuarioAlumno(usuario);
+			}
+			if(usuario.getTipo() == 3) {
+				errores = validarDatosUsuarioTutor(usuario);
+			}
+		}
+		return errores;
+	}
+	
 }
